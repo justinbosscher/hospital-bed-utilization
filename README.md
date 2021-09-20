7483008ec84a0bd1f91a2ea2c3f7fc248a695c66
Justin Bosscher <justinbosscher@gmail.com>
2021-09-19 23:17:23 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Hospital Utilization Survival Analysis
+![Under Construction](https://ps.w.org/easy-under-construction/assets/banner-772x250.png?rev=2417171)
+## Table of Contents
+* [Project Objective](#project-objective)
+* [Methods](#methods)
+* [Technologies](#technologies)
+* [Getting Started](#getting-started)
+* [Project Description](#project-description)
+* [Training Models Results](#training-models-results)
+    * [SOME MODEL](#SOME-MODEL)
+    * [SOME MODEL](#SOME-MODEL)
+    * [SOME MODEL](#SOME-MODEL)
+    * [Model Comparison](#model-comparison)
+* [Test Model Results](#test-model-results)
+    * [SOME MODEL Test Model](#linear-discriminant-analysis-test-model)
+* [License](#license)
+
+## Project Objective
+#### Hopefully, this project will predict bed utilization rates using Google Trends data. Currently, I am still looking for relevant hospital data.
+
+## Methods
+* Data Visualization
+* Survival Analysis
+* Kaplan-Meier Estimator
+* Nelson-Aalen Estimator
+* Log-Rank-Test
+* Cox-Regression
+
+## Technologies
+* Python 3.8.1
+* Python Virtual Environment
+* Jupyter Notebook
+* Python Packages
+    * load_data
+    * requests
+    * pandas
+    * numpy
+    * KaplanMeierFitter
+    * NelsonAalenFitter
+    * logrank_test
+    * CoxPHFitter
+    * matplotlib
+
+## Getting Started
+1. If necessary, install the python3-venv package using the following command:
+    sudo apt install python3.8-venv
+2. Create virtual environment
+    python3 -m venv hospitals-wkspc
+3. cd into the folder created by running the last command.
+4. Activate the environment:
+    source bin/activate
+5. pip install jupyter
+6. pip3 install pytrends
+6. pip install psycopg2-binary
+6. pip install pandas
+7. pip install numpy
+8. pip install matplotlib.pyplot
+9. 
+
+## Project Description
+## Training Models Results
+### SOME MODEL
+### SOME MODEL
+### SOME MODEL
+## Test Model Results
+### SOME MODEL
+
+## License
+Copyright (c) 2021 Justin Bosscher
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ load_data.py
@@ -0,0 +1,175 @@
+# Import modules
+import requests
+import json
+from pytrends.request import TrendReq
+from pytrends.exceptions import ResponseError
+import pandas as pd
+from datetime import date
+from datetime import timedelta
+
+
+# Global variables
+
+# Parameters for pytrends payload
+# Create lists of search terms
+search_terms_1 = ['covid', 'corona virus', 'covid19', 'delta variant', 'vaccine']
+search_terms_2 = ['covid variant', 'corona virus variant', 'corona variant']
+list_of_term_lists = [search_terms_1, search_terms_2]
+COUNTRY = 'US'
+
+# Addresses for CDC and HealthData.gov data
+hospitals_addy = 'https://healthdata.gov/resource/anag-cw7u.json'
+covid_addy = 'https://data.cdc.gov/resource/n8mc-b4w4.json'
+addy_list = []
+addy_list.append(hospitals_addy)
+addy_list.append(covid_addy)
+
+
+def get_json_data(addy):
+    return pd.read_json(addy)
+
+
+def get_trends_data(terms_list, pytrend_obj, params_list):
+    # Build payload one and load data
+    pytrend_obj.build_payload(terms_list, timeframe=params_list[0], geo=params_list[1])
+    # Load regional interest and interest over time data
+    reg_int_terms_df = pytrend_obj.interest_by_region(inc_low_vol=True)
+    iot_terms_df = pytrend_obj.interest_over_time()
+
+    return reg_int_terms_df, iot_terms_df
+
+
+
+def create_data():
+    # Create Trend Request object
+    pytrend = TrendReq()
+    
+    # Create parameters specific to creation of data
+    date_interval_start = '2020-01-01'
+    date_interval_end = str(date.today() - timedelta(days=1))   # Today's data probably isn't complete
+    DATE_INTERVAL = date_interval_start + ' ' + date_interval_end
+    params_list = [DATE_INTERVAL, COUNTRY]
+    # Supposedly, Google returns 400 if search term (s?) is > 100 char's
+    # I am not able to search more than 5 terms w/o the 400 error
+    list_of_dfs = []
+    for term_list in list_of_term_lists:
+        list_of_dfs.extend(get_trends_data(term_list, pytrend, params_list))
+
+    # Combine regional interest data from both builds
+    reg_int_dfs = [list_of_dfs[0], list_of_dfs[2]]
+    reg_int_df = pd.concat(reg_int_dfs, axis=1)
+
+    # Combine interest over time from both builds
+    # Change isPartial column names to reflect the respective search term list
+    list_of_dfs[1].rename({'isPartial': 'isPartial1'}, axis=1, inplace=True)
+    list_of_dfs[3].rename({'isPartial': 'isPartial2'}, axis=1, inplace=True)
+    iot_dfs = [list_of_dfs[1], list_of_dfs[3]]
+    iot_df = pd.concat(iot_dfs, axis=1)
+
+
+    # TODO: Do I need to save snapshots of these data in order to create
+    # a time series?? If so, how?
+    # Load the hospital utilization and surveillance data
+    # For loop through list of addresses from which json data is to be loaded
+    list_of_dfs_from_json = []
+    for addy in addy_list:
+        list_of_dfs_from_json.append(get_json_data(addy))
+
+    # Save data to pandas data frames
+    hospitals_df = list_of_dfs_from_json[0]
+    covid_df = list_of_dfs_from_json[1]
+
+    # TODO: Convert index to datetime objects
+    iot_df.index = pd.to_datetime(iot_df.index.date)
+    #iot_df.index = pd.to_datetime(iot_df.index)
+    #pd.to_datetime(hospitals_df, format="%Y/%m/%d")
+    #pd.to_datetime(covid_df, format="%Y/%m/%d")
+
+    # Save data as csv's
+    reg_int_df.to_csv('assets/reg_interest.csv')
+    iot_df.to_csv('assets/iot.csv')
+    hospitals_df.to_csv('assets/hospitals.csv')
+    covid_df.to_csv('assets/covid.csv')
+
+    list_of_dfs_to_return = [reg_int_df, iot_df, hospitals_df, covid_df]
+
+    # Return data frames
+    return(list_of_dfs_to_return)
+
+
+def update_data():
+
+    # TODO: Do this without loading pandas data frame
+    prev_iot_df = pd.read_csv('assets/iot.csv', index_col=0)
+    last_iot_date = pd.to_datetime(prev_iot_df.tail(1).index)
+    one_week = timedelta(days=7)
+    last_iot_date_plus_week = last_iot_date + one_week
+    yesterday = pd.to_datetime(date.today()) - timedelta(days=1)   # Today's data probably isn't complete
+
+    if yesterday > last_iot_date_plus_week:
+
+        # No need to do anything!
+        return('Data are already up to date.')
+
+    else:
+        # Load the rest of the csv's as df's
+        prev_reg_int_df = pd.read_csv('assets/reg_interest.csv')
+        prev_hospitals_df = pd.read_csv('assets/hospitals.csv')
+        prev_covid_df = pd.read_csv('assets/covid.csv')
+
+        # Create Trend Request object
+        pytrend = TrendReq()
+
+        # Set pytrends parameter values specific to updating data
+        date_interval_start = prev_iot_df.tail(1).index[0]
+        date_interval_end = date.today() - timedelta(days=1)
+        DATE_INTERVAL = str(date_interval_start) + ' ' + str(date_interval_end)
+        print('date interval: ', DATE_INTERVAL)
+        params_list = [DATE_INTERVAL, COUNTRY]
+        print('params: ', params_list)
+
+        # Supposedly, Google returns 400 if search term (s?) is > 100 char's
+        # I am not able to search more than 5 terms w/o the 400 error
+        list_of_dfs = []
+        for term_list in list_of_term_lists:
+            list_of_dfs.extend(get_trends_data(term_list, pytrend, params_list))
+        print(len(list_of_dfs))
+        # Combine regional interest data from both builds
+        reg_int_dfs = [list_of_dfs[0], list_of_dfs[2]]
+        updated_reg_int_df = pd.concat(reg_int_dfs, axis=1)
+
+        # Combine interest over time from both builds
+        # Change isPartial column names to reflect the respective search term list
+        list_of_dfs[1].rename({'isPartial': 'isPartial1'}, axis=1, inplace=True)
+        list_of_dfs[3].rename({'isPartial': 'isPartial2'}, axis=1, inplace=True)
+        iot_dfs = [list_of_dfs[1], list_of_dfs[3]]
+        updated_iot_df = pd.concat(iot_dfs, axis=1)
+
+        # Combine new data with those which were saved previously
+        reg_int_df = pd.concat([prev_reg_int_df, updated_reg_int_df], axis=0)
+        iot_df = pd.concat([prev_iot_df, updated_iot_df], axis=0)
+        iot_df.index = pd.to_datetime(iot_df.index).date
+
+        
+        #TODO: Only load new data, then combine it with previous data
+        # Right now, the previous data are re-loaded
+        # Update CDC and HealthData.gov data
+        # For loop through list of addresses from which json data is to be loaded
+        list_of_dfs_from_json = []
+        for addy in addy_list:
+            list_of_dfs_from_json.append(get_json_data(addy))
+
+        # Save data to pandas data frames
+        hospitals_df = list_of_dfs_from_json[0]
+        covid_df = list_of_dfs_from_json[1]
+
+       
+        # Save data as csv's
+        reg_int_df.to_csv('assets/reg_interest.csv')
+        iot_df.to_csv('assets/iot.csv')
+        hospitals_df.to_csv('assets/hospitals.csv')
+        covid_df.to_csv('assets/covid.csv')
+
+        list_of_dfs_to_return = [reg_int_df, iot_df, hospitals_df, covid_df]
+        # Return data frames
+        return(list_of_dfs_to_return)
